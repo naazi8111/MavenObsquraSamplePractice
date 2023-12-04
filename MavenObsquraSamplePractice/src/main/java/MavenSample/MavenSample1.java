35171228687c236396cb5a74398919cf787569dc
naazi8111 <95483627+naazi8111@users.noreply.github.com>
2023-12-04 10:51:15 +0530

    Add files via upload

---
+++ MavenObsquraSamplePractice/src/main/java/MavenSample/MavenSample1.java
@@ -0,0 +1,11 @@
+package MavenSample;
+
+public class MavenSample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("here is the sample ,maven");
+
+	}
+
+}
