f0c32fbd63b10748b27b5c0c489c768b6789a651
naazi8111 <95483627+naazi8111@users.noreply.github.com>
2023-12-05 09:07:55 +0530

    Update MavenSample1.java

--- MavenObsquraSamplePractice/src/main/java/MavenSample/MavenSample1.java
+++ MavenObsquraSamplePractice/src/main/java/MavenSample/MavenSample1.java
@@ -4,7 +4,7 @@ public class MavenSample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("here is the sample ,maven");
+		//System.out.println("here is the sample ,maven");
 
 	}
 
